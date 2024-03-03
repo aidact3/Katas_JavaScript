c37a1ac583b71a603ccaf23ea0f782cfb77cf3d0
aidact3 <aidacarabot.tosar@gmail.com>
2024-03-03 10:50:09 -0400

    Ejercicio 3

---
+++ Ejericicio 3/index.js
@@ -0,0 +1,35 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+var x =  10;
+var y = 5;
+var z = x * y;
+console.log(z);
+
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+var x = 10;
+var y = 2;
+var z = x / y;
+console.log(z);
+
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+var x = 15;
+var y = 9;
+var z = 15 % 9;
+console.log(z);
+
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+// teniendo dos variables p = 10 y j = 5.
+var p = 10;
+var j = 5;
+var o = p + j;
+console.log(o);
+
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+var c = 10;
+var m = 5;
+var i = c * m;
+console.log(i);
