f303219d6dd85dd158cf6b0f8b2fdb11cf7a64c7
aidact3 <aidacarabot.tosar@gmail.com>
2024-03-07 21:36:12 -0400

    Ejercicio 15

---
+++ ejercicio15.js
@@ -0,0 +1,15 @@
+// Usa includes: Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .includes de javascript.
+
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+function showCamiseta (param){
+  const camisetaWords = []
+  for (i=0; i < param.length; i++){
+    if (param[i].includes("Camiseta")){
+      camisetaWords.push(param[i])
+    }
+  }
+  return camisetaWords
+}
+
+console.log(showCamiseta(products));
